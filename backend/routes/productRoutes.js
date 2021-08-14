180f44cded82cac9c347b9d98b129088ad8b200b
Debbie <debbieellis7@gmail.com>
2021-08-14 16:09:13 +0800

    API routes custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,25 +9,32 @@ const router = express.Router()
  * @route: GET /api/products
  * @access: Public
  */
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({})
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
 
-  res.json(products)
-}))
+    res.json(products)
+  })
+)
 
 /**
  * @desc: Fetch single product
  * @route: GET /api/products/:id
  * @access: Public
  */
-router.get('/:id', asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id)
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
 
-  if (product) {
-    res.json(product)
-  } else {
-    res.status(404).json({ message: 'Product not found' })
-  }
-}))
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
+)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
 
@@ -15,6 +16,10 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
