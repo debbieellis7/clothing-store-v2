412080c180008ef85a33d8a146dc0eff425de20a
Debbie <debbieellis7@gmail.com>
2021-08-22 20:24:32 +0800

    User register reducer, action and screen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,7 +3,10 @@ import {
   USER_LOGIN_REQUESTS,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
-  USER_LOGOUT
+  USER_LOGOUT,
+  USER_REGISTER_REQUESTS,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL
 } from '../constants/userConstants'
 
 export const login = (email, password) => async dispatch => {
@@ -36,4 +39,35 @@ export const logout = () => dispatch => {
   localStorage.removeItem('userInfo')
 
   dispatch({ type: USER_LOGOUT })
+}
+
+export const register = (name, email, password) => async dispatch => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUESTS })
+
+    const config = { headers: { 'Content-Type': 'application/json' } }
+
+    const { data } = await axios.post(
+      '/api/users',
+      { name, email, password },
+      config
+    )
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data
+    })
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data
+    })
+
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message
+    })
+  }
 }
