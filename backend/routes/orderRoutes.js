5107b48345cf639f5564d3d6f0196f680bf2ca92
Debbie <debbieellis7@gmail.com>
2021-09-07 16:49:09 +0800

    Create order

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addOrderItems } from '../controllers/orderController'
+import { addOrderItems } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
